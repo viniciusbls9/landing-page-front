b77f0b5d87107131097c0465b9255540c3ba7d83
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-06-19 12:31:00 -0300

    feat: first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import theme from 'styles/theme';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from 'styles/global';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

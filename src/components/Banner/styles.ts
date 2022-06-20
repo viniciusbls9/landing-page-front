310685508f32b2b495cf1368a652b409631eb82a
vinicius <beneditoloura@gmail.com>
2022-06-20 07:59:26 -0300

    ✨ feat: change banner texts and styles

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,37 +1,63 @@
 import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
 export const BannerWrapper = styled.div`
   ${({ theme }) => css`
     display: grid;
     grid-template-columns: 1fr;
-    height: 100%;
-    min-height: 100vh;
+    height: auto;
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: 75rem 1fr;
+    `}
   `}
 `;
 
 export const LeftBannerWrapper = styled.div`
   ${({ theme }) => css`
     background-color: ${theme.colors.primary};
-    padding: ${theme.spacings.medium} 0 ${theme.spacings.medium}
-      ${theme.spacings.small};
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: ${theme.spacings.medium};
+    
+    ${media.greaterThan('medium')`
+      padding: ${theme.spacings.xxlarge};
+    `}
   `}
 `;
 
 export const BannerTitle = styled.h1`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.xxlarge};
+    color: ${theme.colors.bannerTitleColor};
+    text-align: center;
+    
+    ${media.greaterThan('medium')`
+      text-align: left;
+    `}
   `}
 `;
 
 export const BannerDescription = styled.p`
   ${({ theme }) => css`
-    font-size: ${theme.font.sizes.large};
+    font-size: ${theme.font.sizes.medium};
     margin: ${theme.spacings.xsmall} 0;
+    color: ${theme.colors.bannerDescriptionColor};
+    line-height: ${theme.spacings.small};
+    text-align: center;
+
+    ${media.greaterThan('medium')`
+      text-align: left;
+    `}
   `}
 `;
 
 export const RightBannerWrapper = styled.div`
-  background: url(https://img.freepik.com/fotos-gratis/foto-isolada-de-mulher-usando-aplicativo-de-smartphone-gosta-de-navegar-na-midia-social-cria-conteudo-de-noticias-faz-pedidos-online-usa-oculos-e-poses-casuais-de-macacao-sobre-a-parede-bege-do-estudio_273609-44111.jpg?w=1060&t=st=1655659745~exp=1655660345~hmac=d0d3bef33cfe245674cb366c6380d58ccbac249c52aed1ba1460588ba4cdfe86);
+  display: none;
+  /* background: url(https://img.freepik.com/fotos-gratis/foto-isolada-de-mulher-usando-aplicativo-de-smartphone-gosta-de-navegar-na-midia-social-cria-conteudo-de-noticias-faz-pedidos-online-usa-oculos-e-poses-casuais-de-macacao-sobre-a-parede-bege-do-estudio_273609-44111.jpg?w=1060&t=st=1655659745~exp=1655660345~hmac=d0d3bef33cfe245674cb366c6380d58ccbac249c52aed1ba1460588ba4cdfe86);
   background-size: contain;
-  background-repeat: no-repeat;
+  background-repeat: no-repeat; */
 `;

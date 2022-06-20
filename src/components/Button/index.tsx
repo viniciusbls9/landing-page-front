b0346adffe81f8e3b132ccb8e3253d734f744c0c
vinicius <beneditoloura@gmail.com>
2022-06-20 08:08:01 -0300

    ✨ feat: add new button prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,10 +5,11 @@ import * as S from './styles';
 type Props = {
   children: React.ReactNode;
   href: string;
+  maxWidth?: string
 };
 
-const Button: React.FC<Props> = ({ children, href }) => (
-  <S.ButtonWrapper href={href}>{children}</S.ButtonWrapper>
+const Button: React.FC<Props> = ({ children, href, maxWidth }) => (
+  <S.ButtonWrapper href={href} maxWidth={maxWidth}>{children}</S.ButtonWrapper>
 );
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,18 +1,22 @@
 import styled, { css } from 'styled-components';
 
-export const ButtonWrapper = styled.a`
-  ${({ theme }) => css`
+type ButtonProps = { maxWidth?: string }
+
+export const ButtonWrapper = styled.a<ButtonProps>`
+  ${({ theme, maxWidth }) => css`
     background: ${theme.colors.buttonBackgroundColor};
     border-radius: ${theme.border.radius};
     color: ${theme.colors.buttonColor};
-    font-size: ${theme.font.sizes.xsmall};
-    padding: ${theme.spacings.xxsmall} ${theme.spacings.small};
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xsmall} ${theme.spacings.small};
     text-decoration: none;
     text-align: center;
     white-space: nowrap;
+    max-width: ${maxWidth};
     transition: background 0.3s ease-in-out;
     &:hover {
-      background: ${theme.colors.primaryHover};
+      background: ${theme.colors.buttonBackgroundHover};
+      color: ${theme.colors.buttonHoverColor};
     }
   `}
 `;

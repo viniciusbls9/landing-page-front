4e73beab9e62d5c9916a372f87197de62c3fcd9d
vinicius <beneditoloura@gmail.com>
2022-06-24 07:44:54 -0300

    ✨ feat: create SectionAboutProject component

---
+++ src/components/SectionAboutProject/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
+export const Wrapper = styled.section`
+  ${({ theme }) => css`
+    display: flex;
+    background: ${theme.colors.white};
+    /* transform: skewY(3deg); */
+
+    ${media.greaterThan('medium')`
+      padding: ${theme.spacings.large} 0;
+    `}
+  `}
+`
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: ${theme.spacings.small};
+    /* transform: skewY(-3deg); */
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: 39rem 1fr;
+    `}
+
+    ${media.greaterThan('large')`
+      grid-template-columns: 1fr 1fr;
+    `}
+  `}
+`
+
+export const Image = styled.img`
+  display: block;
+  max-width: min(60rem, 100%);
+
+  ${media.lessThan('medium')`
+    max-width: 100%;
+  `}
+`
+
+export const Text = styled.div`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.medium};
+
+    p {
+      color: ${theme.colors.black};
+      margin-bottom: ${theme.spacings.small};
+    }
+
+    strong {
+      border-bottom: 2px solid ${theme.colors.primary};
+    }
+  `}
+`

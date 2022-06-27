38766e809ffd4c433365a9c77141454d6bf81987
vinicius <beneditoloura@gmail.com>
2022-06-27 07:56:14 -0300

    ✨ feat: create new prop

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,14 +4,16 @@ import * as S from './styles'
 
 export type Props = {
   reverseColor?: boolean
+  as: React.ElementType
   children: string
 }
 
 const Heading: React.FC<Props> = ({
   reverseColor = false,
+  as,
   children
 }) => (
-  <S.Wrapper reverseColor={reverseColor}>
+  <S.Wrapper reverseColor={reverseColor} as={as}>
     {children}
   </S.Wrapper>
 )

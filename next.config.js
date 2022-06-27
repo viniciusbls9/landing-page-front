0f2814ab8238dbc177c9af20788a9e54c91f7ab5
vinicius <beneditoloura@gmail.com>
2022-06-27 08:14:38 -0300

    ✨ feat: add next domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://cdn-icons-png.flaticon.com', 'cdn-icons-png.flaticon.com']
+  }
 }
 
 module.exports = nextConfig

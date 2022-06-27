0a57bf3debf45a1320662296c549ae238541d4c9
vinicius <beneditoloura@gmail.com>
2022-06-27 07:56:25 -0300

    ✨ feat: add new components on index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as SectionHero } from './SectionHero';
 export { default as Container } from './Container';
+export { default as CardModule } from './CardModule';
+export { default as Heading } from './Heading';
 export { default as Button } from './Button';
 export { default as SectionAbout } from './SectionAbout';
 export { default as SectionServices } from './SectionServices';
